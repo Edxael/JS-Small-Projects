dc748a9284312de2e3153739bbd869834d37a7bc
Edxael <zerphyr.r@gmail.com>
2017-02-13 01:44:41 -0700

    Update on Math

--- 00-test-js/js-code.js
+++ 00-test-js/js-code.js
@@ -1,21 +1,8 @@
 console.log("Hello");
 
-var st_buton = document.getElementById("st");
-var sp_buton = document.getElementById("sp");
-var time_ele = document.getElementById("time");
-
-var interval;
-var d = new Date();
+var a = "5";
+var b = Math.sqrt(a);
+var c = 0;
 
 
-st_buton.addEventListener("click", function(){
-    var timePas = 0;
-    interval = setInterval(function(){
-        timePas += 1;
-        time_ele.innerHTML = timePas;
-    }, 1000);
-});
-
-sp_buton.addEventListener("click", function(){
-    clearInterval(interval);
-});
+console.log(b);
