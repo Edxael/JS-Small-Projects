82530039ba6de7ed6ca9bb44d320dda38004ae27
Edxael <zerphyr.r@gmail.com>
2017-02-18 16:35:01 -0700

    Writing test for: JavaScript code:

---
+++ 00-test-js/holder.js
@@ -0,0 +1,8 @@
+
+
+var addTo = function(passed){
+    var inner = 2;
+    return passed + inner;
+};
+
+console.log(addTo(3));
--- 00-test-js/js-code.js
+++ 00-test-js/js-code.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+console.log("Hello 7");
+console.log("");
+
+    // Declare a function
+function addTo(passed){
+
+        // Declare a function withing a Functions A.K.A. = "CLOSURE"
+    function add(inner){
+        return passed + inner;
+    }
+
+    return add;
+}
+
+   //  Creating 2 new instances of the Function: addTo()
+var add_7 = new addTo(7);
+var add_4 = new addTo(4);
+
+
+    // Passing a numerical parameter to the Newly created instances.
+console.log(add_7(1));  // 8
+console.log(add_4(1));  // 5
