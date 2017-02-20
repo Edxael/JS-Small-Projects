5a68a09de95d8914ef14c0752f78b2fedc237de7
Edxael <zerphyr.r@gmail.com>
2017-02-20 11:56:31 -0700

    update on app V10

--- 11-vanilla-To-Do-App/01-V10/code.js
+++ 11-vanilla-To-Do-App/01-V10/code.js
@@ -7,29 +7,41 @@ var td_obj = {
 
         // Data Base, with all task and completion status
     list: [
-        {task:"Morning Exersice", completed: true},
+        {task:"Morning Exersice", completed: false},
         {task:"CodeWars - GitHub", completed: true},
         {task:"Eat BreakFast", completed: false},
-        {task:"Turn On Computer", completed: true}
+        {task:"Turn On Computer", completed: false}
     ],
 
         // Will display the Data Base on the Console
     displayTD: function(){
-        var newTask = document.createElement("li");
-        var taskHolder = document.getElementById("task-ul");
-        document.getElementById("task-ul").innerHTML = "";
+
+        var taskHolder = document.getElementById("task-div-m");
+        document.getElementById("task-div-m").innerHTML = "";
+
+        // var newTask = document.createElement("div");
+
 
         if(this.list.length === 0){
+            var newTask = document.createElement("div");
             newTask.innerText = "The To-Do lIst is empty";
             taskHolder.insertBefore(newTask, taskHolder.childNodes[0]);
         }
 
         else{
             for(var x = 0; x < this.list.length; x++){
-                var mark = (this.list[x].completed === true) ? "(x) " : "( ) ";
-                var loopTask = document.createElement("li");
-                loopTask.innerText = mark + this.list[x].task;
+                // var mark = (this.list[x].completed === true) ? "(x) " : "( ) ";
+                var check = (this.list[x].completed === true) ? true : false;
+                var loopTask = document.createElement("div");
+                loopTask.className = "gral-div-task";
+
+
+                // loopTask.innerText = mark + this.list[x].task;
+                loopTask.appendChild(this.createRad(check, x));
+                loopTask.appendChild(this.createP(this.list[x].task));
                 loopTask.appendChild(this.createDelButton(x));
+
+
                 taskHolder.insertBefore(loopTask, taskHolder.childNodes[0]);
             }
         }
@@ -57,17 +69,19 @@ var td_obj = {
     },
 
         // Will delete: Element (task, completed) from Data Base
-    deleteTD: function(a){
-        this.list.splice(a, 1);
+    deleteTD: function(x){
+        console.log("Index to Delete: " + x);
+        this.list.splice(x, 1);
         this.displayTD();
     },
 
         // Will change the current status of "completed" the the oposite: true or false
-    taskComp: function(){
-        var cmpT = document.getElementById("comp-inp").valueAsNumber;
-        this.list[cmpT].completed = !this.list[cmpT].completed;
+    taskComp: function(x){
+        // var cmpT = document.getElementById("comp-inp").valueAsNumber;
+        // this.list[cmpT].completed = !this.list[cmpT].completed;
+        this.list[x].completed = !this.list[x].completed;
         this.displayTD();
-        document.getElementById("comp-inp").value = "";
+        // document.getElementById("comp-inp").value = "";
     },
 
         // if one or more task are "falce" -> will change all to "true",   -and-   if all taks are "true" --> will change all to "false"
@@ -82,16 +96,39 @@ var td_obj = {
     createDelButton: function(x){
         var delBu = document.createElement("button");
         delBu.innerHTML = "Delete";
-        delBu.className = "li-del";
+        delBu.className = "gral-but";
+
+        delBu.setAttribute('onClick', "td_obj.deleteTD(" + x + ")");
+
         delBu.id = x;
         return delBu;
+    },
+
+    createRad: function(check, x){
+        var radBu = document.createElement("input");
+        radBu.type = "radio";
+        console.log("Check: " + check);
+        radBu.checked = check;
+        radBu.className = "gral-rad";
+        radBu.setAttribute('onClick', "td_obj.taskComp(" + x + ")");
+
+        return radBu;
+    },
+
+    createP: function(text){
+        var par1 = document.createElement("p");
+        par1.innerText = text;
+        par1.className = "gral-para";
+        // par1.text-decoration = line-through;
+        return par1;
     }
+
 };
 
     // single Event Listener for all Items in the lis using: "Event Delegation"
-var task_ul = document.getElementById("task-ul");
-task_ul.addEventListener("click", function(event1){
-    var buton2del = event1.target.id;
-    console.log(buton2del);
-    td_obj.deleteTD(buton2del);
-});
+// var task_ul = document.getElementById("task-div-m");
+// task_ul.addEventListener("click", function(event1){
+//     var buton2del = event1.target.id;
+//     console.log(buton2del);
+//     td_obj.deleteTD(buton2del);
+// });
